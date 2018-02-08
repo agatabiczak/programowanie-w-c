74898d2e41d695c127f0d7590b9e1ef20e25627a
Agata Biczak <36275625+agatabiczak@users.noreply.github.com>
2018-02-08 18:47:51 +0100

    Create walec.c

---
+++ walec.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+ float promien, wysokosc, objetosc;
+ promien = 3.3;
+ wysokosc = 44.4;
+ objetosc = 3.1415926 * promien * promien * wysokosc;
+printf("Objetosc walca = %f", objetosc);
+}
